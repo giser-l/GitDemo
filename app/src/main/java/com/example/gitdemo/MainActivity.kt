b51172516ce84cbec5b123621abb96358c5e59bc
晓涵 刘 <2723744600@qq.com>
2021-11-19 15:43:58 +0800

    implement: buttonAdd onClick listener

--- app/src/main/java/com/example/gitdemo/MainActivity.kt
+++ app/src/main/java/com/example/gitdemo/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.gitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+    private var number = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val button = findViewById<Button>(R.id.buttonAdd)
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = "$number"
+        button.setOnClickListener {
+            textView.text = "${number++}"
+        }
     }
 }
