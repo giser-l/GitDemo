788d3e74c501ca027113a5c6ad253ae6a54ef69d
晓涵 刘 <2723744600@qq.com>
2021-11-19 21:55:31 +0800

    implement: options menu reset item selected

--- app/src/main/java/com/example/gitdemo/MainActivity.kt
+++ app/src/main/java/com/example/gitdemo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.gitdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 import org.w3c.dom.Text
@@ -26,6 +27,14 @@ class MainActivity : AppCompatActivity() {
         buttonSub.setOnClickListener {
             textView.text = "${--number}"
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menuReset){
+            number = 0
+            textView.text = "$number"
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
